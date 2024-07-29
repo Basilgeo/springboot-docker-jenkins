7976a00e9358fc041652c2cce2b74ead79e0c480
unknown <basilgeorge2002@gmail.com>
2024-07-29 15:40:22 +0530

    second

--- src/main/java/com/ust/springboot_docker_jenkins/controller/DockerController.java
+++ src/main/java/com/ust/springboot_docker_jenkins/controller/DockerController.java
@@ -1,7 +1,9 @@
 package com.ust.springboot_docker_jenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DockerController {
 
     @GetMapping("/docker")
